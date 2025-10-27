b4cadac6a457e262bfc945bbc3079ca20383d282
manidll <emmanuel.rivas240691@potros.itson.edu.mx>
2025-10-26 21:18:41 -0700

    Se agrego pantalla de inicio y se arreglo error en HomeActivity.kt respecto a llamdo de boton.

--- app/src/main/java/com/example/geos/HomeActivity.kt
+++ app/src/main/java/com/example/geos/HomeActivity.kt
@@ -3,14 +3,15 @@ package com.example.geos
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.LinearLayout
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val btnCaptura = findViewById<LinearLayout>(R.id.btnCaptura)
+        val btnCaptura = findViewById<Button>(R.id.btnCaptura)
+
 
         btnCaptura.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
